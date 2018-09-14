efea8cff5853c848fff3cf03455caafc627147d4
Anders-Biro <41286308+Anders-Biro@users.noreply.github.com>
2018-09-14 15:23:38 -0600

    Added event to Send and View button

--- Chatter/app/src/main/java/ca/nait/gschenk/chatter/ChatterSendActivity.java
+++ Chatter/app/src/main/java/ca/nait/gschenk/chatter/ChatterSendActivity.java
@@ -6,6 +6,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatterSendActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -15,12 +28,17 @@ public class ChatterSendActivity extends AppCompatActivity implements View.OnCli
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatter_send);
 
+        //this is a hack to allow us to use the main UI thread
+        //will be removed when we learn how to multithread.
         if (android.os.Build.VERSION.SDK_INT > 9)
         {
             StrictMode.ThreadPolicy policy =
                     new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
+        Button buttonSend = (Button)findViewById(R.id.button_send_data);
+        buttonSend.setOnClickListener(this);
+
         Button buttonView = (Button)findViewById(R.id.button_view_data);
         buttonView.setOnClickListener(this);
     }
@@ -28,8 +46,40 @@ public class ChatterSendActivity extends AppCompatActivity implements View.OnCli
     @Override
     public void onClick(View view)
     {
+        switch(view.getId())
+        {
+            case R.id.button_send_data:
+            {
+                EditText editText = (EditText)findViewById(R.id.edittext_send_chat);
+                String chatter = editText.getText().toString();
+                postToChatter(chatter);
+                editText.setText("");
+                break;
+            }
+
+        }
         Intent intent = new Intent(this, ChatterReceiveActivity.class);
         this.startActivity(intent);
+
+    }
+
+    private void postToChatter(String chatter)
+    {
+        try
+        {
+            HttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost("http://www.youcode.ca/JitterServlet");
+            List<NameValuePair> formParameters = new ArrayList<NameValuePair>();
+            formParameters.add(new BasicNameValuePair("DATA", chatter));
+            formParameters.add(new BasicNameValuePair("LOGIN_NAME", "Ders"));
+            UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(formParameters);
+            post.setEntity(formEntity);
+            client.execute(post);
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(this, "Error: " + e, Toast.LENGTH_LONG).show();
+        }
     }
 }
 
