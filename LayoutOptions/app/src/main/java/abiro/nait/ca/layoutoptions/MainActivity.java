612c235edb40fc6590298899e9bc0db3d7fbc1ce
Anders-Biro <41286308+Anders-Biro@users.noreply.github.com>
2018-09-27 15:53:41 -0600

    LayoutOptions Demo

---
+++ LayoutOptions/app/src/main/java/abiro/nait/ca/layoutoptions/MainActivity.java
@@ -0,0 +1,14 @@
+package abiro.nait.ca.layoutoptions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative);
+    }
+}
