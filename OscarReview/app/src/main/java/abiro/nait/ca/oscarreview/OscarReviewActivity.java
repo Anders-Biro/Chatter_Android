d9412bf54fe1a8819bb67091a266feeada78c6df
Anders-Biro <41286308+Anders-Biro@users.noreply.github.com>
2018-10-14 00:37:26 -0600

    Oscar Review Lab

---
+++ OscarReview/app/src/main/java/abiro/nait/ca/oscarreview/OscarReviewActivity.java
@@ -0,0 +1,37 @@
+package abiro.nait.ca.oscarreview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+
+import java.io.BufferedReader;
+
+public class OscarReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oscar_review);
+    }
+
+    private void postOscarReview
+    {
+        BufferedReader = in = null;
+        try
+        {
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request =
+        }
+        catch
+        {
+
+        }
+
+        }
+    }
+}
